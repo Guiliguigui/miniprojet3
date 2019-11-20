37af797adc98efa2442257e4df0f0601475407b7
Andrp99 <andreas.pierre@etud.u-picardie.fr>
2019-11-20 16:37:14 +0100

    Simplification du main avec des fonctions, debogage de la partie update.

--- DBRegister.py
+++ DBRegister.py
@@ -10,24 +10,103 @@ import logging
 import os
 import argparse
 import csv
+import sys
 from datetime import datetime
 
 def close_connection(conection):
     if conection:
         conection.close()
 
+def create_SIV(cursor):
+    """
+    Create the SIV table.
+
+    Parameters
+    ----------
+    cursor : sqlite3.crusor
+        the cursor udsed to execute the creation request.
+    """
+
+    cursor.execute('''CREATE TABLE SIV (
+        adresse_titulaire           TEXT    NOT NULL,
+        nom                         TEXT    NOT NULL,
+        prenom                      TEXT    NOT NULL,
+        immatriculation             TEXT    NOT NULL,
+        date_immatriculation        TEXT    NOT NULL,
+        vin                         TEXT    NOT NULL,
+        marque                      TEXT    NOT NULL,
+        denomination_commerciale    TEXT    NOT NULL,
+        couleur                     TEXT    NOT NULL,
+        carrosserie                 TEXT    NOT NULL,
+        categorie                   TEXT    NOT NULL,
+        cylindree                   TEXT    NOT NULL,
+        energie                     TEXT    NOT NULL,
+        places                      TEXT    NOT NULL,
+        poids                       TEXT    NOT NULL,
+        puissance                   TEXT    NOT NULL,
+        type                        TEXT    NOT NULL,
+        variante                    TEXT    NOT NULL,
+        version                     TEXT    NOT NULL)''')
+
+def update_vehicle(row):
+    """
+    Update a specific row in the SIV table with a corresponding row from the csv file.
+
+    Parameters
+    ----------
+    row : dict
+        A specific row from the csv file where all the vehicle data is stocked.
+    """
+    values=tuple(row.values())
+    values=values+(row['immatriculation'],)
+    cursor.execute('''UPDATE SIV SET 
+    adresse_titulaire           = ?,
+    nom                         = ?,
+    prenom                      = ?,
+    immatriculation             = ?,
+    date_immatriculation        = ?,
+    vin                         = ?,
+    marque                      = ?,
+    denomination_commerciale    = ?,
+    couleur                     = ?,
+    carrosserie                 = ?,
+    categorie                   = ?,
+    cylindree                   = ?,
+    energie                     = ?,
+    places                      = ?,
+    poids                       = ?,
+    puissance                   = ?,
+    type                        = ?,
+    variante                    = ?,
+    version                     = ?
+    WHERE immatriculation=?''', values)
+
+def insert_vehicle(row):
+    """
+    Insert a specific row in the SIV table from the csv file.
+
+    Parameters
+    ----------
+    row : dict
+        A specific row from the csv file where all the vehicle data is stocked.
+    """
+    values=tuple(row.values())
+    cursor.execute('INSERT INTO SIV VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', values)
+
+
 
 if __name__ == "__main__":
     log_file_name = 'DBRegister-' + datetime.today().strftime('%Y-%m-%d') + '.log'
     logging.basicConfig(filename=log_file_name, level=logging.INFO)
 
-    logging.info('Started')
     parser = argparse.ArgumentParser()
     parser.add_argument('infile')
     args=parser.parse_args()
+    logging.info('Started')
 
     if not os.path.isfile(args.infile):
-        logging.error('Argument is not a file.')
+        logging.error('ERROR 101: \'%s\' is not a file!' % args.infile)
+        sys.exit(101)
     else:
         connection=sqlite3.connect('IBM_Db2.sqlite3')
         logging.info('Connected to the database.') 
@@ -40,29 +119,9 @@ if __name__ == "__main__":
     if cursor.fetchone()[0]==1 :
 	    logging.info('Table SIV exists.')
     else:
-        cursor.execute('''CREATE TABLE SIV (
-            adresse_titulaire           TEXT    NOT NULL,
-            nom                         TEXT    NOT NULL,
-            prenom                      TEXT    NOT NULL,
-            immatriculation             TEXT    NOT NULL,
-            date_immatriculation        TEXT    NOT NULL,
-            vin                         TEXT    NOT NULL,
-            marque                      TEXT    NOT NULL,
-            denomination_commerciale    TEXT    NOT NULL,
-            couleur                     TEXT    NOT NULL,
-            carrosserie                 TEXT    NOT NULL,
-            categorie                   TEXT    NOT NULL,
-            cylindree                   TEXT    NOT NULL,
-            energie                     TEXT    NOT NULL,
-            places                      TEXT    NOT NULL,
-            poids                       TEXT    NOT NULL,
-            puissance                   TEXT    NOT NULL,
-            type                        TEXT    NOT NULL,
-            variante                    TEXT    NOT NULL,
-            version                     TEXT    NOT NULL)''')
+        create_SIV(cursor)
         connection.commit()
         logging.info('Table SIV created.')
-
 
     csvfile = open(args.infile, 'r', newline='')
     logging.info('Csv file openned.')
@@ -76,16 +135,11 @@ if __name__ == "__main__":
         cursor.execute(' SELECT COUNT(immatriculation) FROM SIV WHERE immatriculation=? ', values)
         if cursor.fetchone()[0]==1 :
             updated+=1
-            values=()
-            for item in row.items():
-                values=values + (item[0],item[1],)
-            values=values+(row['immatriculation'],)
-            #logging.info(values)
-            #cursor.execute('UPDATE SIV SET ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=?, ?=? WHERE immatriculation=?', values)
+            update_vehicle(row)
         else:
             inserted+=1
-            values=tuple(row.values())
-            cursor.execute('INSERT INTO SIV VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', values)
+            insert_vehicle(row)
+            
 
     logging.info('%d vehicle inserted, %d vehicle updated.', inserted, updated)
 
